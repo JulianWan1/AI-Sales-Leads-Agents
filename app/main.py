834af08f01abaee8ef476de03c97281422518d1b
julianwan <julianwan.jwwj@gmail.com>
2026-05-27 10:35:08 +0800

    Enable user to change lead status via dropdown

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.api.lead_discovery import router as lead_router
 from app.api.lead_enrichment import router as enrichment_router
 from app.api.lead_scoring import router as scoring_router
 from app.api.outreach_strategy import router as strategy_router
+from app.api.leads import router as leads_router
 from app.db.init_db import init_db
 
 app = FastAPI()
@@ -16,6 +17,7 @@ app.include_router(lead_router)
 app.include_router(enrichment_router)
 app.include_router(scoring_router)
 app.include_router(strategy_router)
+app.include_router(leads_router, prefix="/leads", tags=["leads"])
 
 
 @app.get("/")
